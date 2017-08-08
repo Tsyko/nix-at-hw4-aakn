c0861ea2304a6759e26aa95efa88890756f3b700
Andriy Tsykholyas <andriy.tsykholyas@gmail.com>
2017-08-08 22:45:56 +0300

    small fixup

--- src/main/java/JsonPackage/JsonReader.java
+++ src/main/java/JsonPackage/JsonReader.java
@@ -1,20 +1,17 @@
 package JsonPackage;
 
 import com.google.gson.Gson;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class JsonReader {
 
-    public static StudentList read() {
+    public static StudentList read() throws FileNotFoundException {
 
         Gson gson = new Gson();
-        BufferedReader br;
-        br = null;
-        try {
-            br = new BufferedReader(new FileReader("src/main/resources/students.json"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/students.json"));
 
         StudentList response = gson.fromJson(br, StudentList.class);
         return response;
--- src/main/java/com/nix/at/jtp/DreamTeamApp.java
+++ src/main/java/com/nix/at/jtp/DreamTeamApp.java
@@ -3,18 +3,23 @@ package com.nix.at.jtp;
 import JsonPackage.JsonReader;
 import JsonPackage.StudentList;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class DreamTeamApp {
 
     public static void main(String[] args) {
-
-        //TODO: add exception handling
-        StudentList studentList = JsonReader.read();
-        //studentList.sort(new StudentComparator());
-
+        try {
+            StudentList studentList = JsonReader.read();
+            List<Student> list = Arrays.asList(studentList.getStudents());
+            list.sort(new StudentComparator());
+            print(list);
+        } catch (Exception e) {
+            //TODO: add exception handling
         }
+    }
 
-    private static void print(Student[] students) {
-        // TODO: add exception handling
-
+    private static void print(List<Student> list) {
+        // TODO: print list of students
     }
 }
